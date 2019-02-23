4e665bf258d9947c5ebe9d1ddb50b6f369c1870e
Paul Tobin <tobster111@gmail.com>
2019-02-23 10:40:05 +0000

    Remove render() and all instances of this

--- src/components/CharPicker.js
+++ src/components/CharPicker.js
@@ -32,21 +32,21 @@ const CharPicker = props => {
       });
   }
 
-  render() {
+
     let content = <p>Loading characters...</p>;
 
     if (
-      !this.state.isLoading &&
-      this.state.characters &&
-      this.state.characters.length > 0
+      !isLoading &&
+      characters &&
+      characters.length > 0
     ) {
       content = (
         <select
-          onChange={this.props.onCharSelect}
-          value={this.props.selectedChar}
-          className={this.props.side}
+          onChange={props.onCharSelect}
+          value={props.selectedChar}
+          className={props.side}
         >
-          {this.state.characters.map(char => (
+          {characters.map(char => (
             <option key={char.id} value={char.id}>
               {char.name}
             </option>
@@ -54,13 +54,13 @@ const CharPicker = props => {
         </select>
       );
     } else if (
-      !this.state.isLoading &&
-      (!this.state.characters || this.state.characters.length === 0)
+      !isLoading &&
+      (!characters || characters.length === 0)
     ) {
       content = <p>Could not fetch any data.</p>;
     }
     return content;
-  }
+  
 }
 
 export default CharPicker;
